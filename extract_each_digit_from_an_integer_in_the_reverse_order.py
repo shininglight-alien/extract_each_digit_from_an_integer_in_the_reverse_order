5abcfb7bb751360961977d218fcd48b200c85376
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 13:22:17 +0800

    final

--- extract_each_digit_from_an_integer_in_the_reverse_order.py
+++ extract_each_digit_from_an_integer_in_the_reverse_order.py
@@ -3,5 +3,8 @@
 def reverse_digits(n):
     return ' '.join(list(str(n))[::-1])
 
-# Test the function
-print(reverse_digits(7536))
+# get user input
+n = int(input("Integer Entry:"))
+
+# test the function
+print(reverse_digits(n))
